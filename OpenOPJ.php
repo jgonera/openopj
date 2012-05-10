a011dab57b584c1423fe0ce7ea864643b1766b5e
Juliusz Gonera <juliusz_g@iwonka.med.virginia.edu>
2012-05-10 17:50:54 -0400

    very basic docs and tests

--- OpenOPJ.php
+++ OpenOPJ.php
@@ -1,8 +1,26 @@
 <?php
-class OpenOPJ {
+namespace OpenOPJ;
 
-    function __construct($filename) {
-        //
+class OpenOPJException extends \Exception {}
+class FileReadError extends OpenOPJException {}
+
+class OpenOPJ {
+    public $worksheets;
+    protected $fileHandle;
+
+    function __construct($fileName) {
+        $this->fileHandle = @fopen($fileName, 'rb');
+        if (!$this->fileHandle) throw new FileReadError("Can't read file $fileName");
+        flock($this->fileHandle, LOCK_SH);
+
+        $this->parse();
+
+        flock($this->fileHandle, LOCK_UN);
+        fclose($this->fileHandle);
+    }
+
+    protected function parse() {
+
     }
 }
 ?>
--- test/OpenOPJTest.php
+++ test/OpenOPJTest.php
@@ -1,14 +1,21 @@
 <?php
 require_once('OpenOPJ.php');
 
+use OpenOPJ\OpenOPJ;
+
 class OpenOPJTest extends PHPUnit_Framework_TestCase {
 
     protected function setUp() {
         $this->opj = new OpenOPJ('support/test.opj');
     }
 
+    public function testFileReadError() {
+        $this->setExpectedException('OpenOPJ\FileReadError');
+        $opj = new OpenOPJ('support/nonexistent.opj');
+    }
+
     public function testWorksheetCount() {
-        $this->assertCount(4, $this->opj->worksheet);
+        $this->assertCount(4, $this->opj->worksheets);
     }
 }
 ?>
